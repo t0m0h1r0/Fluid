47c468570b5c2eff155dc0c30503a39fc94aea90
Your Name <you@example.com>
2025-02-05 03:16:28 +0900

    .

--- v4/compact_solver.py
+++ v4/compact_solver.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.sparse import diags
+from scipy.sparse import diags, lil_matrix
 from scipy.sparse.linalg import spsolve
 
 class CompactSolver:
@@ -9,48 +9,74 @@ class CompactSolver:
     
     def _setup_matrices(self):
         """コンパクトスキームの係数行列を設定"""
-        N = self.config.Nx
-        h = self.config.dx
+        Nx, Ny, Nz = self.config.Nx, self.config.Ny, self.config.Nz
+        dx = self.config.dx
         
         # 4次精度コンパクトスキーム
-        alpha = 1/4  # コンパクトスキームのパラメータ
-        a = (3/2) / h  # 右辺の係数
+        alpha = 1/4
+        a = (3/2) / dx
         
-        # 主対角と副対角の設定
-        main_diag = np.ones(N)
-        off_diag = alpha * np.ones(N-1)
+        # 各方向の行列を初期化
+        self.A = []
+        self.A_periodic = []
+        self.B = []
+        self.B_periodic = []
         
-        # 周期境界条件の実装
-        self.A = diags([main_diag, off_diag, off_diag],
-                      [0, 1, -1], format='csr')
-        self.A_periodic = self.A.copy()
-        self.A_periodic[0, -1] = alpha
-        self.A_periodic[-1, 0] = alpha
-        
-        # 右辺の差分作用素
-        self.B = diags([0, a, -a],
-                      [0, 1, -1], format='csr')
-        self.B_periodic = self.B.copy()
-        self.B_periodic[0, -1] = -a
-        self.B_periodic[-1, 0] = a
+        for N in [Nx, Ny, Nz]:
+            # 主対角と副対角
+            main_diag = np.ones(N)
+            off_diag = alpha * np.ones(N-1)
+            
+            # 通常の行列
+            A = lil_matrix((N, N))
+            A.setdiag(main_diag)
+            A.setdiag(off_diag, k=1)
+            A.setdiag(off_diag, k=-1)
+            
+            # 周期境界条件の行列
+            A_periodic = A.copy()
+            A_periodic[0, -1] = alpha
+            A_periodic[-1, 0] = alpha
+            
+            # 右辺の差分作用素
+            B = lil_matrix((N, N))
+            B.setdiag(np.zeros(N))
+            B.setdiag(a * np.ones(N-1), k=1)
+            B.setdiag(-a * np.ones(N-1), k=-1)
+            
+            # 周期境界条件
+            B_periodic = B.copy()
+            B_periodic[0, -1] = -a
+            B_periodic[-1, 0] = a
+            
+            # CSR形式に変換して保存
+            self.A.append(A.tocsr())
+            self.A_periodic.append(A_periodic.tocsr())
+            self.B.append(B.tocsr())
+            self.B_periodic.append(B_periodic.tocsr())
     
     def solve_pressure(self, p, rhs):
         """圧力のポアソン方程式を解く"""
-        for i in range(self.config.Ny):
-            for j in range(self.config.Nz):
-                p[:, i, j] = spsolve(self.A_periodic, rhs[:, i, j])
-        return p
+        Nx, Ny, Nz = self.config.Nx, self.config.Ny, self.config.Nz
+        result = np.zeros_like(p)
+        
+        for i in range(Ny):
+            for j in range(Nz):
+                result[:, i, j] = spsolve(self.A_periodic[0], rhs[:, i, j])
+        
+        return result
     
     def derivative(self, f, axis=0, boundary='periodic'):
         """指定方向の空間微分を計算"""
         if boundary == 'periodic':
-            A = self.A_periodic
-            B = self.B_periodic
+            A = self.A_periodic[axis]
+            B = self.B_periodic[axis]
         else:
-            A = self.A
-            B = self.B
+            A = self.A[axis]
+            B = self.B[axis]
         
         result = np.zeros_like(f)
+        
         if axis == 0:
             for i in range(self.config.Ny):
                 for j in range(self.config.Nz):
@@ -66,4 +92,5 @@ class CompactSolver:
                 for j in range(self.config.Ny):
                     rhs = B @ f[i, j, :]
                     result[i, j, :] = spsolve(A, rhs)
+        
         return result
